0324e082e2fad81cfc15dc32a743545ea30ef910
MayThuKyaw237 <maythu.mtkyaw@gmail.com>
2025-06-23 23:18:19 +0700

    advenced_mult.py

---
+++ cell03/ex03/advanced_mult.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+i = 0
+while i <= 10:
+    j = 0
+    print(f"Table de {i}: ", end = "")
+    while j <= 10:
+        multiplication = i * j
+        print(multiplication, end = " ")
+        j += 1
+    print()
+    i += 1
+    
