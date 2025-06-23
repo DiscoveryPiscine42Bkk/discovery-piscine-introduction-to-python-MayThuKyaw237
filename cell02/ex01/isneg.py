95cd10003c7681c8a5624167e043e60af1551ca1
MayThuKyaw237 <maythu.mtkyaw@gmail.com>
2025-06-23 15:26:20 +0700

    isneg.py

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+inp_num = int(input(""))
+if inp_num < 0:
+    print("This number is negative.")
+elif inp_num > 0:
+    print("This number is positive.")
+elif inp_num == 0:
+    print("This number is both positive and negative.")
